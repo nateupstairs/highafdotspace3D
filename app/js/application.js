715acf12a604d8ab79e9526cd67a5e41c1d5afd2
nateupstairs <nate.upstairs@gmail.com>
2016-04-23 16:34:08 -0700

    [camera] camera animation

--- app/js/application.js
+++ app/js/application.js
@@ -2,6 +2,7 @@ import {Engine as Engine} from './engine'
 import {Ship as Ship} from './ship'
 import {Loader as Loader} from './loader'
 import {Listener as Listener} from './listener'
+import {Camera as Camera} from './camera'
 
 async function start() {
   var loader = new Loader()
@@ -10,13 +11,16 @@ async function start() {
 
   var listener = new Listener()
   var engine = new Engine()
+  var camera = new Camera(engine.camera)
   var ship = new Ship(assets)
   listener.register(ship)
+  listener.register(camera)
 
   engine.camera.position.x = 1000
   engine.camera.up = new THREE.Vector3(0, 1, 0)
   engine.camera.lookAt(new THREE.Vector3(0, 0, 0))
   engine.addEntity(0, ship)
+  engine.updateList.push(camera)
 
   DEBUG.ship = ship
 }
---
+++ app/js/camera.js
@@ -0,0 +1,66 @@
+/**
+ * Engine
+ */
+
+var _ = require('lodash')
+var easing = require('./easing')
+
+export class Camera {
+
+  constructor(camera) {
+    this.camera = camera
+    this.time = 0
+    this.animationTime = 5
+    this.progress = 0
+    this.lookAt = new THREE.Vector3(0, 0, 0)
+    this.positions = {
+      pos1: {
+        position: new THREE.Vector3(90, 0, 0),
+        lookAt: new THREE.Vector3(8, 0, 0)
+      },
+      pos2: {
+        position: new THREE.Vector3(-90, 0, 0),
+        lookAt: new THREE.Vector3(-8, 0, 0)
+      }
+    }
+    this.target = 'pos1'
+    this.resetMove = false
+    this.startPos
+  }
+
+  feedEvent(data) {
+    console.log('EVENT TEST!')
+    console.log(data)
+    if (data.data.camera) {
+      this.target = data.data.camera
+      this.resetMove = true
+    }
+  }
+
+  updateCameraPosition() {
+    let progress = easing.easeInOutCubic(this.progress)
+
+    if (this.startPos) {
+      // this.startPos has the originating camera location
+      // this.positions[this.target] has target position/lookat
+      // set camera position / lookat to match
+    }
+  }
+
+  update(delta) {
+    this.time += delta
+    if (this.resetMove) {
+      this.progress = 0
+      this.startPos = {
+        position: Object.assign({}, this.camera.position),
+        rotation: Object.assign({}, this.camera.rotation)
+      }
+      this.resetMove = false
+    }
+    this.progress += delta / this.animationTime
+    if (this.progress < 1) {
+      this.updateCameraPosition()
+    }
+  }
+
+}
